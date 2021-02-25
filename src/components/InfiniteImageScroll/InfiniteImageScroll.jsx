61212ffc07ff6f3f55080d842d7777ac7fa4a16f
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-25 03:20:46 -0500

    tutorial #4: image lazy loading w/ hooks

--- src/components/InfiniteImageScroll/InfiniteImageScroll.jsx
+++ src/components/InfiniteImageScroll/InfiniteImageScroll.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 
-import './index.css'
+import { useFetch, useInfiniteScroll, useLazyLoading } from './customHooks'
+import './index.css';
 
 function App() {
   const imgReducer = (state, action) => {
@@ -13,26 +14,24 @@ function App() {
         return state;
     }
   }
-  const [imgData, imgDispatch] = useReducer(imgReducer,{ images:[], fetching: true})
-  // next code block goes here
-  
-  // make API calls
-  useEffect(() => {
-    imgDispatch({ type: 'FETCHING_IMAGES', fetching: true })
-    fetch('https://picsum.photos/v2/list?page=0&limit=10')
-      .then(data => data.json())
-      .then(images => {
-        imgDispatch({ type: 'STACK_IMAGES', images })
-        imgDispatch({ type: 'FETCHING_IMAGES', fetching: false })
-      })
-      .catch(e => {
-        // handle error
-        imgDispatch({ type: 'FETCHING_IMAGES', fetching: false })
-        return e
-      })
-  }, [ imgDispatch ])
 
-  // next code block goes here
+  const pageReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADVANCE_PAGE':
+        return { ...state, page: state.page + 1 }
+      default:
+        return state;
+    }
+  }
+
+  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 })
+  const [imgData, imgDispatch] = useReducer(imgReducer, { images: [], fetching: true, })
+
+  let bottomBoundaryRef = useRef(null);
+  useFetch(pager, imgDispatch);
+  useLazyLoading('.card-img-top', imgData.images)
+  useInfiniteScroll(bottomBoundaryRef, pagerDispatch);
+
   return (
     <div className="">
       <nav className="navbar bg-light">
@@ -42,6 +41,7 @@ function App() {
           </a>
         </div>
       </nav>
+
       <div id='images' className="container">
         <div className="row">
           {imgData.images.map((image, index) => {
@@ -51,8 +51,9 @@ function App() {
                 <div className="card-body ">
                   <img
                     alt={author}
+                    data-src={download_url}
                     className="card-img-top"
-                    src={download_url}
+                    src={'https://picsum.photos/id/870/300/300?grayscale&blur=2'}
                   />
                 </div>
                 <div className="card-footer">
@@ -63,6 +64,13 @@ function App() {
           })}
         </div>
       </div>
+
+      {imgData.fetching && (
+        <div className="text-center bg-secondary m-auto p-3">
+          <p className="m-0 text-white">Getting images</p>
+        </div>
+      )}
+      <div id='page-bottom-boundary' style={{ border: '1px solid red' }} ref={bottomBoundaryRef}></div>
     </div>
   );
 }
